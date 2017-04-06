c458e19c18299cb205fc367664446d15cd130fae
adziobek <adziobek@apreel.com>
2017-04-06 10:26:46 +0200

    Stub(), operator przypisania wartości >>

--- src/test/java/pl/SimpleTest.groovy
+++ src/test/java/pl/SimpleTest.groovy
@@ -9,10 +9,19 @@ class SimpleTest extends Specification {
 
     def "should return 2 from first element of list"() {
         given:
-         List<Integer> list = new ArrayList<>()
+        List<Integer> list = new ArrayList<>()
         when:
         list.add(1)
         then:
         2 == list.get(0)
     }
+
+    def "should return size 3"() {
+        given:
+        List list = Stub()
+        list.size() >> 3
+        expect:
+        list.size() == 3
+    }
+
 }
